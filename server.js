da97d3f427b2a804cddcca10dec187615ae54dbf
CaiDucTien <caiductien@gmail.com>
2026-05-16 23:05:04 +0700

    fix: update CORS headers to include Authorization

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ const writeLog = (msg) => {
 const server = http.createServer((req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
     if (req.method === "OPTIONS") 
         return res.end();
